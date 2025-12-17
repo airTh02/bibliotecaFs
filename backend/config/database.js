022c445c0a5726a6f7b26933ce6b48db65eb5707
thiago <thiagorerezende@gmail.com>
2025-12-17 18:58:46 -0300

    :rewind: Alterado login do banco.

--- backend/config/database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize("biblioteca", "biblioteca", "Microsoftazure77", {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false
-});
-
-export default sequelize
