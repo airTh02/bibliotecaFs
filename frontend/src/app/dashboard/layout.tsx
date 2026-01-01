e2494832a03ddd2e4d749a2adebff915f3ede0dd
airTh02 <thiagorerezende@gmail.com>
2025-12-31 21:37:37 -0300

    :bug: Adicionado verificações de token (refresh token) (#1)

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,13 +1,13 @@
 "use client"
 
+import { AuthProvider } from "@/context/authContext";
 import { DashboardProvider } from "@/context/dashboardContext";
 import { Toaster } from "sonner";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <DashboardProvider>
-      {children}
-    
-    </DashboardProvider>
+      <DashboardProvider>
+        {children}
+      </DashboardProvider>
   );
 }
